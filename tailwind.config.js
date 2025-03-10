2e6d017b3adaa3cc3b15bfbf77ad5254a815096d
Saurabh Bahadur <133571086+saurabhbahadur@users.noreply.github.com>
2025-03-10 16:54:07 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary_bg: "#000000",
+          secondary_bg: "#1F2937",
+          primary_text: "#ffffff",
+          secondary_text: "#d1d5db",
+          hover_color: "#9ca3af",
+          icon : "#ff0000",
+        },
+        backgroundImage: {
+          "primary_button_grad": "linear-gradient(to right, #374151, #111827)", // Gray gradient
+          "secondary_button_grad": "linear-gradient(to right, #5f1f80, #f52828)", //
+        },
+      },
+    },
+    plugins: [],
+  }
+  
